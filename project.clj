1e4969993dd1ffe9e16e84a4a76003cc36e860ed
Carmen La <carmen.la@mail.utoronto.ca>
2014-12-05 10:26:33 -0500

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 -                 [clj-rss "0.1.9"]
--                 [me.raynes/fs "1.4.4"]
+-                 [me.raynes/fs "1.4.6"]
 -                 [crouton "0.1.2"]
 -                 [cheshire "5.3.1"]
--                 [clj-text-decoration "0.0.2"]
--                 [io.aviso/pretty "0.1.12"]
+-                 [clj-text-decoration "0.0.3"]
+-                 [io.aviso/pretty "0.1.13"]
 -                 [hiccup "1.0.5"]
--                 [selmer "0.7.3"]
+                  [selmer "0.7.7"]
 -                 [markdown-clj "0.9.58"
 -                  :exclusions [com.keminglabs/cljx]]]])
