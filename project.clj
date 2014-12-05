6aca634f89e1958fb428fb87bda9c26f528f63f5
lacarmen <carmen.la@mail.utoronto.ca>
2014-12-05 16:50:40 -0500

    Bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.1"
+(defproject cryogen-core "0.1.2"
             :description "Cryogen's compiler"
             :url "https://github.com/lacarmen/cryogen-core"
             :license {:name "Eclipse Public License"
