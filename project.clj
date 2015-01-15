fee46ddf5484c55b17182a13b1bf2969fe0c238c
Yogthos <dmitri.sotnikov@gmail.com>
2015-01-14 22:07:51 -0500

    updated to use init function to load plugins

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.16"
+(defproject cryogen-core "0.1.17"
   :description "Cryogen's compiler"
   :url "https://github.com/lacarmen/cryogen-core"
   :license {:name "Eclipse Public License"
--- src/cryogen_core/plugins.clj
+++ src/cryogen_core/plugins.clj
@@ -1,12 +1,14 @@
 (ns cryogen-core.plugins
   (:require [cryogen-core.compiler :refer [compile-assets-timed]]
             [clojure.edn :as edn]
+            [clojure.string :as s]
             [text-decoration.core :refer :all]))
 
 (defn load-plugin [url]
-  (let [config (edn/read-string (slurp url))]
-    (println (green (str "loading module: " (:description config))))
-    (require (:namespace config))))
+  (let [{:keys [description init]} (edn/read-string (slurp url))]
+    (println (green (str "loading module: " description)))
+    (-> init str (s/split #"/") first symbol require)
+    ((resolve init))))
 
 (defn load-plugins []
   (let [plugins (.getResources (ClassLoader/getSystemClassLoader) "plugin.edn")]
