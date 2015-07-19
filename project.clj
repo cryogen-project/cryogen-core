85ea98f0ffa552eee3d9fb6f48bfb105bf0944dd
Carmen La <carmen.la@mail.utoronto.ca>
2015-07-19 10:48:51 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.6.0"]
+            :dependencies [[org.clojure/clojure "1.7.0"]
                            [clj-rss "0.1.9"]
                            [me.raynes/fs "1.4.6"]
                            [crouton "0.1.2"]
@@ -11,6 +11,6 @@
                            [clj-text-decoration "0.0.3"]
                            [io.aviso/pretty "0.1.18"]
                            [hiccup "1.0.5"]
-                           [selmer "0.8.2"]
+                           [selmer "0.8.5"]
                            [pandect "0.5.2"]
                            [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]])
