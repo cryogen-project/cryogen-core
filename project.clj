2df8fdec316ccadd5193b08dca35b95bccba93c2
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-09-23 08:13:37 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0"]
-                           [clj-rss "0.2.0"]
+                           [clj-rss "0.2.3"]
                            [me.raynes/fs "1.4.6"]
                            [crouton "0.1.2"]
                            [cheshire "5.5.0"]
                            [clj-text-decoration "0.0.3"]
                            [io.aviso/pretty "0.1.18"]
                            [hiccup "1.0.5"]
-                           [selmer "0.8.8"]
+                           [selmer "0.9.2"]
                            [pandect "0.5.2"]
                            [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]])
