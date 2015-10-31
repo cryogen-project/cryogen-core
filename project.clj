da1fa73f8cdb79cf5bc80b356922fb11c346ad28
Carmen La <carmen.wla@gmail.com>
2015-10-31 14:51:19 -0400

    bump up dependencies and version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.25"
+(defproject cryogen-core "0.1.26"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
@@ -9,8 +9,8 @@
                            [crouton "0.1.2"]
                            [cheshire "5.5.0"]
                            [clj-text-decoration "0.0.3"]
-                           [io.aviso/pretty "0.1.18"]
+                           [io.aviso/pretty "0.1.19"]
                            [hiccup "1.0.5"]
-                           [selmer "0.9.2"]
-                           [pandect "0.5.2"]
+                           [selmer "0.9.3"]
+                           [pandect "0.5.4"]
                            [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]])
