1fb621e6edf1684836a8fca28445ba4ba72431b7
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-11 10:09:44 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.30"
+(defproject cryogen-core "0.1.31"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
