c5c3d08d8a47f2d10fe030dca06ab36753a6e47c
Arsene Rei <arsene.rei@gmail.com>
2016-05-27 00:51:01 -0700

    Revert "Revert "Fallback to resources/templates/{pages,posts}""
    This reverts commit 9cac040c0fd96b30ae409c8a2c5e0dcb3489289c.

--- test/cryogen_core/compiler_test.clj
+++ test/cryogen_core/compiler_test.clj
@@ -1,6 +1,9 @@
 (ns cryogen-core.compiler-test
   (:require [clojure.test :refer :all]
-            [cryogen-core.compiler :refer :all]))
+            [cryogen-core.compiler :refer :all]
+            [cryogen-core.markup :as m]
+            [me.raynes.fs :as fs])
+  (:import [java.io File]))
 
 ; Test that the content-until-more-marker return nil or correct html text.
 (deftest test-content-until-more-marker
@@ -21,3 +24,43 @@ and more content.
          "<div id=\"post\"><div class=\"post-content\">
     this post has more marker
 </div></div>")))
+
+(defn- markdown []
+  (reify m/Markup
+    (dir [this] "md")
+    (ext [this] ".md")))
+
+(defn- create-entry [dir file]
+  (fs/mkdirs (File. dir))
+  (fs/create (File. (str dir File/separator file))))
+
+(defn- reset-resources []
+  (fs/delete-dir "resources")
+  (create-entry "resources" ".gitkeep"))
+
+(defn- check-for-pages [mu]
+  (find-pages {:page-root "pages"} mu))
+
+(defn- check-for-posts [mu]
+  (find-posts {:post-root "posts"} mu))
+
+(deftest test-find-entries
+  (reset-resources)
+  (let [mu (markdown)]
+    (testing "Finds no files"
+      (is (empty? (check-for-posts mu))
+      (is (empty? (check-for-pages mu))))
+
+    (let [dir->file
+          [[check-for-posts "resources/templates/md/posts" "post.md"]
+           [check-for-posts "resources/templates/posts" "post.md"]
+           [check-for-pages "resources/templates/md/pages" "page.md"]
+           [check-for-pages "resources/templates/pages" "page.md"]]]
+      (doseq [[check-fn dir file] dir->file]
+        (testing (str "Finds files in " dir)
+          (create-entry dir file)
+          (let [entries (check-fn mu)]
+            (is (= 1 (count entries)))
+            (is (= (.getAbsolutePath (File. (str dir File/separator file)))
+                   (.getAbsolutePath (first entries)))))
+          (reset-resources)))))))
