78e4a6d06779719a3f903a8f371663e34c406b41
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-01-02 14:47:54 +0100

    fixed navbar-pages bug

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.47-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/cryogen-core "0.1.47-SNAPSHOT"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
