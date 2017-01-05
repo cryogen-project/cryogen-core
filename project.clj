65cd1f5997d21aa418c24ed77e42d4aa3e81f177
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-01-05 17:35:34 +0100

    adjust project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.1.47"
+(defproject org.domaindrivenarchitecture/cryogen-core "0.1.48-SNAPSHOT"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
