1b7f0fb367f3b851de700bef6e1c50fcd0015ff1
Carmen La <carmen.wla@gmail.com>
2017-01-16 23:29:48 -0500

    update deps and version number

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cryogen-core "0.1.47"
+(defproject cryogen-core "0.1.48"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
-                           [cheshire "5.6.3"]
+                           [cheshire "5.7.0"]
                            [clj-rss "0.2.3"]
                            [clj-text-decoration "0.0.3"]
                            [enlive "1.1.6"]
@@ -13,6 +13,6 @@
                            [io.aviso/pretty "0.1.33"]
                            [me.raynes/fs "1.4.6"]
                            [pandect "0.6.1"]
-                           [selmer "1.10.3"]]
+                           [selmer "1.10.5"]]
             :deploy-repositories [["snapshots" :clojars]
                                   ["releases" :clojars]])
