49ae87f55b6013cd56916e50206a6895d7e7cf51
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-11-22 11:35:47 -0300

    Enlive already has a enlive->hiccup fn for convenience

--- test/cryogen_core/util_test.clj
+++ test/cryogen_core/util_test.clj
@@ -1,17 +1,12 @@
 (ns cryogen-core.util-test
   (:require [cryogen-core.util :refer :all]
+            [net.cgrand.enlive-html :as enlive]
             [clojure.test :refer [deftest testing is are]]))
 
-;; For testing convenience.
-(defn elt
-  "Returns an enlive style html element."
-  ([tag] (elt tag nil))
-  ([tag attrs & content]
-   {:tag tag, :attrs attrs, :content content}))
-
 (deftest filter-html-elems-test
-  (is (= [(elt :div {:class "x"} :content [(elt :div {:class "x"} "foo")])
-          (elt :div {:class "x"} "foo")])
+  (is (= (enlive/html [:div {:class "x"} [:div {:class "x"} "foo"]]
+                      [:div {:class "x"} "foo"]))
       (filter-html-elems (comp #{"x"} :class :attrs)
-                         [(elt :h1 {:class "y"} "things!")
-                          (elt :div {:class "x"} (elt :div {:class "x"} "foo"))])))
+                         (enlive/html
+                          [:h1 {:class "y"} "things!"]
+                          [:div {:class "x"} [:div {:class "x"} "foo"]]))))
