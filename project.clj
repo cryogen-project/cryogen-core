594593058d94c6125eefc674bc967015b8e62128
Carmen La <carmen.wla@gmail.com>
2018-10-10 11:26:18 -0400

    bump version and deps

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject cryogen-core "0.1.61"
+(defproject cryogen-core "0.1.63"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [camel-snake-kebab "0.4.0"]
-                           [cheshire "5.8.0"]
+                           [cheshire "5.8.1"]
                            [clj-rss "0.2.3"]
                            [clj-text-decoration "0.0.3"]
                            [enlive "1.1.6"]
                            [hawk "0.2.11"]
                            [hiccup "1.0.5"]
-                           [io.aviso/pretty "0.1.34"]
+                           [io.aviso/pretty "0.1.35"]
                            [me.raynes/fs "1.4.6"]
                            [pandect "0.6.1"]
-                           [prismatic/schema "1.1.7"]
-                           [selmer "1.11.2"]]
+                           [prismatic/schema "1.1.9"]
+                           [selmer "1.12.2"]]
             :deploy-repositories [["snapshots" :clojars]
                                   ["releases" :clojars]])
