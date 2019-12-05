d1a81acc5e2e664f13164ce061091781634bf630
Carmen La <carmen.wla@gmail.com>
2019-12-05 12:59:21 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.2.1"
+(defproject cryogen-core "0.2.2"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
