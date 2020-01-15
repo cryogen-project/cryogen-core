7d721ddc45111e7e3e347236f3f5630e3653333b
Jakub Holy <jakub.holy@telia.no>
2020-01-15 12:35:37 +0100

    Upgrade outdated dependencies, fix Selmer path
    + send correct absolute file: URL to Selmer as it now requires
      (see https://github.com/yogthos/Selmer/issues/213), using the
      correct fn for that

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject cryogen-core "0.3.0"
+(defproject cryogen-core "0.3.1-SNAPSHOT"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.0"]
-                           [camel-snake-kebab "0.4.0"]
-                           [cheshire "5.8.1"]
+                           [camel-snake-kebab "0.4.1"]
+                           [cheshire "5.9.0"]
                            [clj-rss "0.2.5"]
                            [clj-text-decoration "0.0.3"]
                            [enlive "1.1.6"]
@@ -14,7 +14,7 @@
                            [io.aviso/pretty "0.1.37"]
                            [me.raynes/fs "1.4.6"]
                            [pandect "0.6.1"]
-                           [prismatic/schema "1.1.10"]
-                           [selmer "1.12.12"]]
+                           [prismatic/schema "1.1.12"]
+                           [selmer "1.12.18"]]
             :deploy-repositories [["snapshots" :clojars]
                                   ["releases" :clojars]])
