83d26343b296dc34815ef9f1e3ed20446bc95e9c
Dorab Patel <dorabpatel@gmail.com>
2020-12-05 16:59:04 -0800

    Update version to 0.4.1-SNAPSHOT
    Prepare for next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.4.0"
+(defproject cryogen-core "0.4.1-SNAPSHOT"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
