3d4b990b50710c299b300c76b929bb0bbc07fbf6
Yogthos <yogthos@gmail.com>
2026-05-31 22:46:56 -0400

    Update all dependencies to latest versions
    - org.clojure/clojure: 1.11.3 → 1.12.5
    - camel-snake-kebab: 0.4.2 → 0.4.3
    - cheshire: 5.10.0 → 6.2.0
    - clj-rss: 0.2.6 → 0.4.0
    - com.novemberain/pantomime: 2.11.0 → 3.1.0
    - org.clojars.simon_brooke/real-name: 1.0.1 → 1.0.2
    - pandect: 0.6.1 → 1.0.2
    - prismatic/schema: 1.1.12 → 1.4.1
    - selmer: 1.12.61 → 1.13.1
    
    - Removed org.clj-commons/pretty (unused; code uses clojure.pprint)
    - Added io.aviso/pretty 1.4.4 (was transitive of org.clj-commons/pretty;
      provides io.aviso.exception used by compiler.clj)
    
    - Unchanged: clj-text-decoration, enlive, hiccup (1.0.5 stays; 2.0 breaks
      escaping semantics), me.raynes/fs, net.mikera/imagez

--- project.clj
+++ project.clj
@@ -3,23 +3,23 @@
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.11.3"]
-                           [camel-snake-kebab "0.4.2"]
-                           [cheshire "5.10.0"]
-                           [clj-rss "0.2.6"]
+            :dependencies [[org.clojure/clojure "1.12.5"]
+                           [camel-snake-kebab "0.4.3"]
+                           [cheshire "6.2.0"]
+                           [clj-rss "0.4.0"]
                            [clj-text-decoration "0.0.3"]
                            ;; used for mime type inference
-                           [com.novemberain/pantomime "2.11.0" :exclusions [selmer]]
+                           [com.novemberain/pantomime "3.1.0" :exclusions [selmer]]
                            [enlive "1.1.6"]
                            [hiccup "1.0.5"]
-                           [org.clj-commons/pretty "2.6.0"]
+                           [io.aviso/pretty "1.4.4"]
                            [me.raynes/fs "1.4.6"]
                            ;; used in infer-meta to extract dimensions from images
                            [net.mikera/imagez "0.12.0"]
                            ;; real name extraction
-                           [org.clojars.simon_brooke/real-name "1.0.1"]
-                           [pandect "0.6.1"]
-                           [prismatic/schema "1.1.12"]
-                           [selmer "1.12.61"]]
+                           [org.clojars.simon_brooke/real-name "1.0.2"]
+                           [pandect "1.0.2"]
+                           [prismatic/schema "1.4.1"]
+                           [selmer "1.13.1"]]
             :deploy-repositories [["snapshots" :clojars]
                                   ["releases" :clojars]])
