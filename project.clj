ab238ae94e2d0296846f1834a540c16c7ac33339
Yogthos <yogthos@gmail.com>
2026-05-31 23:04:56 -0400

    bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-core "0.4.9"
+(defproject cryogen-core "0.5.0"
             :description "Cryogen's compiler"
             :url "https://github.com/cryogen-project/cryogen-core"
             :license {:name "Eclipse Public License"
